9a43891d71c7714c822749dc9c714e52feb3eb0e
sfuxy2283 <41281780+sfuxy2283@users.noreply.github.com>
2018-10-24 00:23:05 +0900

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export default {
   apiGateway: {
-    REGION: "ap-northeast-2",
-    URL: "https://8rfh9wl7z1.execute-api.ap-northeast-2.amazonaws.com/dev"
+    REGION: "YOUR-API-GATEWAY-REGION",
+    URL: "YOUR-API-GATEWAY-END-POINT-URL"
   },
   cognito: {
-    REGION: "ap-northeast-2",
-    USER_POOL_ID: "ap-northeast-2_kTszjVV1l",
-    APP_CLIENT_ID: "7cf3mut011k6gbol5rqlgc50vk",
-    IDENTITY_POOL_ID: "ap-northeast-2:e2988b6d-97a7-43f3-82b4-05fc161aa2f5"
+    REGION: "YOUR-COGNITO-REGION",
+    USER_POOL_ID: "YOUR-COGNITO-USER-POOL-ID",
+    APP_CLIENT_ID: "YOUR-COGNITO-APP-CLIENT-ID",
+    IDENTITY_POOL_ID: "YOUR-COGNITO-IDENTITY-POOL-ID"
   }
 };
